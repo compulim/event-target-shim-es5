bfb011fa5e69c440f38f83fbb0ce37d3d2fc345b
William Wong <compulim@users.noreply.github.com>
2021-07-13 22:50:50 -0700

    Bump dependencies (#18)
    * Bump deps
    
    * Add entry
    
    * Add Node.js 16.x

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,17 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Changed
+
+- Bumped dependencies in PR [#18](https://github.com/compulim/event-target-shim-es5/pull/18)
+   - [`@babel/core@7.14.6`](https://npmjs.com/package/@babel/core)
+   - [`@babel/preset-env@7.14.7`](https://npmjs.com/package/@babel/preset-env)
+   - [`babel-loader@8.2.2`](https://npmjs.com/package/babel-loader)
+   - [`event-target-shim@6.0.2`](https://npmjs.com/package/event-target-shim)
+   - [`jest@27.0.6`](https://npmjs.com/package/jest)
+   - [`webpack-cli@4.7.2`](https://npmjs.com/package/webpack-cli)
+   - [`webpack@4.46.0`](https://npmjs.com/package/webpack)
+
 ## [1.2.0] - 2020-08-06
 
 ### Changed
--- __tests__/simple.js
+++ __tests__/simple.js
@@ -1,9 +1,46 @@
-import EventTarget, { defineEventAttribute } from '../lib/index';
+import EventTargetFromRoot, {
+  defineCustomEventTarget,
+  defineEventAttribute,
+  Event,
+  EventTarget,
+  getEventAttributeValue,
+  setErrorHandler,
+  setEventAttributeValue,
+  setWarningHandler
+} from '../lib/index';
+
+test('EventTarget from default export should be same as named export', () => {
+  expect(EventTargetFromRoot).toBe(EventTarget);
+});
 
 test('EventTarget should be a class', () => {
   expect(new EventTarget()).toBeTruthy();
 });
 
+test('Event should be a class', () => {
+  expect(new Event()).toBeTruthy();
+});
+
+test('defineCustomEventTarget should be a function', () => {
+  expect(typeof defineCustomEventTarget).toBe('function');
+});
+
 test('defineEventAttribute should be a function', () => {
   expect(typeof defineEventAttribute).toBe('function');
+});
+
+test('getEventAttributeValue should be a function', () => {
+  expect(typeof getEventAttributeValue).toBe('function');
+});
+
+test('setErrorHandler should be a function', () => {
+  expect(typeof setErrorHandler).toBe('function');
+});
+
+test('setEventAttributeValue should be a function', () => {
+  expect(typeof setEventAttributeValue).toBe('function');
+});
+
+test('setWarningHandler should be a function', () => {
+  expect(typeof setWarningHandler).toBe('function');
 });
